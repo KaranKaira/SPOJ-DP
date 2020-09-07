c6223f63ae2cbadc87e90b7b507ddb91148df5c5
Karan Kaira <46125192+KaranKaira@users.noreply.github.com>
2020-09-08 00:10:27 +0530

    Create DIEHARD.cpp

---
+++ DIEHARD.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int dp[10001][10001][4];
+int cal(int h, int a, int place)
+{
+	if (h <= 0 || a <= 0 ) return 0;
+	if (dp[h][a][place] != -1) return dp[h][a][place];
+	int &ans  = dp[h][a][place];
+	if (place == 1)
+		ans = 1 + max(cal(h - 5, a - 10, 2), cal(h - 20, a + 5, 3));
+	if (place == 2)
+		ans = 1 + max(cal(h + 3, a + 2, 1) , cal(h - 20, a + 5, 3));
+	if (place == 3)
+		ans = 1 + max(cal(h + 3, a + 2, 1) , cal(h - 5, a - 10, 2));
+
+	return ans;
+}
+
+
+
+void  solve()
+{
+
+	int h, a; cin >> h >> a;
+	memset(dp, -1, sizeof(dp));
+	cout << max({ cal(h + 3, a + 2, 1) , cal(h - 5, a - 10, 2) , cal(h - 20, a + 5, 3) }) << endl;
+
+}
+
+int main()
+{	int t; cin >> t; while (t--)
+		solve();
+}
